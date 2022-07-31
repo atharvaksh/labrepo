faff6b0ded29a15810e2d0c4e6f7b146e25f5e51
atharvaksh <109942400+atharvaksh@users.noreply.github.com>
2022-07-31 09:57:45 +0530

    Update HelloGitHub.java
    second update from web

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -3,6 +3,8 @@ public class HelloGitHub{
   public static void main (String args[]){
       
       System.out.println("hello from git hub");
+      System.out.println("second line print");
+      
   
   }
 
